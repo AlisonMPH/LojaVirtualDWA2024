3760792033ad79cbbc1985ef6a3a97d83452f31e
Alison <alisonmozer@gmail.com>
2024-08-21 15:57:31 -0300

    ajustado carrinho - botoes de diminuir/aumentar/deletar - atualizar valor total

--- sql/pedido_sql.py
+++ sql/pedido_sql.py
@@ -32,6 +32,12 @@ SQL_ATUALIZAR_PARA_FECHAR = """
     WHERE id=?
 """
 
+SQL_ATUALIZAR_VALOR_TOTAL = """
+    UPDATE pedido
+    SET valor_total=?
+    WHERE id=?
+"""
+
 SQL_EXCLUIR = """
     DELETE FROM pedido
     WHERE id=?
@@ -50,7 +56,7 @@ SQL_OBTER_QUANTIDADE = """
 """
 
 SQL_OBTER_POR_PERIODO = """
-    SELECT id, data_hora, valor_total, endereco_entrega, estado, id_cliente
+    SELECT id, data_formatada, valor_total, endereco_entrega, estado, id_cliente
     FROM pedido
     WHERE (id_cliente = ?) AND (data_hora BETWEEN ? AND ?)
     ORDER BY data_hora DESC
